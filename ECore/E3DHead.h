b03aeed31ed50c7f492bd92c7e27f7918e98556d
zqj <582132116@qq.com>
2022-05-20 17:31:46 +0800

    update

---
+++ ECore/E3DHead.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace E3D
+{
+	struct EColor;//颜色
+	struct EVector2D;//二维向量
+	struct EVector3D;//三维向量
+	struct EVector4D;//四维向量
+	struct EMatrix44;//4×4矩阵
+	struct EVertex4D;//顶点
+	struct EPolyon4D;//基于顶点多边形
+	struct EPolyonF4D;//基于索引多边形
+	struct EObject4D;//对象
+	struct EPlane3D;//三维平面
+	struct ERenderList4D;//渲染列表
+	struct EFrustum;//透视体
+}
---
+++ ECore/EMatrix.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ECommon.h"
+
+namespace E3D
+{
+	//矩阵MATRIX4X4_TYP
+	struct EMatrix44
+	{
+		static EMatrix44 IDENTITY;
+		EFloat m[4][4];
+
+		EMatrix44()
+		{
+			memset(m, )
+		}
+	};
+}
+
+
